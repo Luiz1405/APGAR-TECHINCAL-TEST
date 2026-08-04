cab97a19a4c71d231c110e373bce99b7b08151dc
Luiz Kubaszewski <luiz.kubaszewski@edu.pucrs.br>
2026-08-04 15:21:16 -0300

    feat(estrutura-inicial): Adiciona estrutura inicial do projeto.

---
+++ src/main/java/com/apgar/techinical_test/TechinicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.apgar.techinical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechinicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apgar/techinical_test/TechinicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apgar.techinical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechinicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
